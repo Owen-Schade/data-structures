68a95435b43fd626485e78b4e71d57d28951933a
ocschade <ocschade@stu.naperville203.org>
2025-09-11 13:09:39 -0500

    ts

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,7 @@
+import java.util.HashMap;
 import java.util.Scanner;
-. . .
+import java.util.Set;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,7 +12,7 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        HashMap<String, String> gBook = new HashMap<>();
 
         boolean done = false;
         while(!done)
@@ -19,20 +21,34 @@ public class Gradebook
             String input = in.next().toUpperCase();
             if (input.equals("Q"))
             {
+
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("Please enter name and grade: ");
+                gBook.put(in.next(), in.next());
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.println("Please enter name: ");
+                String n = in.next();
+                if(gBook.containsKey(n)){
+                    gBook.remove(n);
+                }
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("Please enter name: ");
+                String n = in.next();
+                if(gBook.containsKey(n)){
+                    System.out.println("Please enter grade: ");
+                    gBook.put(n, in.next());
+                }
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> keys = gBook.keySet();
+                for (String key: keys){
+                    System.out.println(key+": "+gBook.get(key));
+                }
             } else
             {
                 done = true;
--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -12,5 +12,17 @@ public class MapDemo
     public static void main(String[] args)
     {
         System.out.println("gurt");
+        Map<String, Color> favColors = new HashMap<>();
+        favColors.put("Sreeram", Color.GREEN);
+        favColors.put("Kaitlyn", Color.GREEN);
+        favColors.put("Cam", Color.BLUE);
+        favColors.put("Nimai", Color.RED);
+
+        favColors.put("Cam", Color.ORANGE);
+
+        Set<String> keys = favColors.keySet();
+        for (String key: keys){
+            System.out.println(key+"("+key.hashCode()+"): "+favColors.get(key));
+        }
     }
 }
